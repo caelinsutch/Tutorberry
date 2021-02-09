774ef92e529dd806c9e0c9673c39adfe4671e091
mindy-kim <39883887+mindy-kim@users.noreply.github.com>
2021-02-09 00:46:01 -0800

    Add files via upload

---
+++ src/pages/student.js
@@ -0,0 +1,22 @@
+import React from "react";
+import HowItWorks from "../sections/student/HowItWorks";
+import AvailableSubjects from "../sections/student/AvailableSubjects";
+import Reviews from "../sections/student/Reviews";
+import Faq from "../sections/student/Faq";
+import CTA from "../sections/student/CTA";
+import PageWrapper from "../components/PageWrapper";
+
+const student = () => {
+  return (
+    <>
+      <PageWrapper>
+        <HowItWorks />
+        <AvailableSubjects />
+        <Reviews />
+        <Faq />
+        <CTA />
+      </PageWrapper>
+    </>
+  );
+};
+export default student;
